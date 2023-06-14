27880841d986373a762eaeeae030b9902a863796
Devan Shah <devan0795@gmail.com>
2023-06-14 00:23:06 -0700

    web scraper pipeline

---
+++ pipeline.py
@@ -0,0 +1,89 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import config 
+
+AK = config.api_key
+response_API =  requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={AK}").json()
+
+NBC_url = "https://www.nbcnews.com/politics/donald-trump/updates-trump-arraignment-florida-classified-documents-rcna88871"
+CNN_url = "https://www.cnn.com/2023/06/13/politics/karine-jean-pierre-hatch-act/index.html"
+FOX_url = "https://www.foxnews.com/politics/trump-slams-sham-federal-indictment-election-inference-most-heinous-abuse-power-in-us-history"
+BBC_url = "https://www.bbc.com/news/world-asia-65894721"
+NPR_url = "https://www.npr.org/2023/06/14/1182071288/las-vegas-golden-knights-win-stanley-cup"
+
+titles = []
+for item in response_API['articles']:
+    titles.append(item['title'])
+
+
+def extract_CNN(url):
+    response = requests.get(url)
+    html_content = response.text
+    filtered_content = extract_ld_json(html_content)
+    start = filtered_content.find("articleBody")
+    end = filtered_content.find("articleSection")
+    text = filtered_content[start + 14:end-3]
+    print(text)
+
+def extract_ld_json(html_content, regex):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    script_pattern = re.compile(r'^<script[^>]+type=[\'"]application/ld\+json[\'"]>', re.IGNORECASE)
+    content = ''
+    for script in soup.find_all('script'):
+        if script_pattern.match(str(script)):
+            content += script.string.strip()
+            break
+    return content
+
+def extract_FOX(url):
+    response = requests.get(url)
+    html_content  = response.text
+    start = html_content.find("\"articleBody\":")
+    end = html_content.find("\"datePublished\":")
+    text = html_content[start +  16:end - 4]
+    pattern = r'\b[A-Z]+\b'
+    filtered_text = re.sub(pattern, lambda match: '' if len(match.group()) > 1 else match.group(), text)
+    return filtered_text
+
+def extract_NBC(url):
+    response = requests.get(url)
+    html_content  = response.text
+    start = html_content.find("\"articleBody\":")
+    end = html_content.find("\"dateCreated\":")
+    text = html_content[start +  15:end - 4]
+    return text.replace("\n",  "")
+
+def extract_BBC(url):
+    response = requests.get(url)
+    content = response.text
+    data = []
+    first = content.find("<div class=\"ssrcss-68pt20-Text-TextContributorName e8mq1e96\">")
+    content = content[first:]
+    while content.find("<p class=\"ssrcss-1q0x1qg-Paragraph eq5iqo00\">") != -1:
+        start = content.find("<p class=\"ssrcss-1q0x1qg-Paragraph eq5iqo00\">")
+        content = content[start + 45:]
+        cur_end = content.find("</p></div></div>")
+        data.append(content[:cur_end])
+        content = content[cur_end:]
+    ret = " ".join(data[:-1])
+    term = ret.find(">")
+    return(ret[term+1: ])
+
+def extract_NPR(url):
+    response = requests.get(url)
+    content = response.text
+    cur_end = content.find("<p>")
+    content = content[cur_end + 3:]
+    cur_end = content.find("<p>")
+    content = content[cur_end-3:]
+    data = []
+    while content.find("<p>") != -1:
+        start = content.find("<p>")
+        content = content[start + 3:]
+        cur_end = content.find("</p>")
+        data.append(content[:cur_end])
+        content = content[cur_end:]
+    ret = " ".join(data)
+    return ret
+print(extract_NPR(NPR_url))
