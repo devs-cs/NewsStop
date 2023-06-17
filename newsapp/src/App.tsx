ab6078db8140e0115367bd196d3426e97661db76
Hang Pham <hangtpham04@gmail.com>
2023-06-16 21:02:09 -0500

    fix(tsx): config ts and debug issues

--- newsapp/src/App.tsx
+++ newsapp/src/App.tsx
@@ -1,55 +1,55 @@
 import './App.css';
-// import { Theme, ThemeProvider } from '@mui/material/styles';
-// import CssBaseline from '@mui/material/CssBaseline';
 import { Row, Col } from 'antd';
-
 import React, { useEffect, useState } from 'react';
-
-import {NewsCard} from './component/NewsCard.tsx';
-
-// import { firebaseConfig } from './app/firebase';
-
+import NewsCard  from './component/NewsCard';
+import {CardProps}  from './component/NewsCard';
 import {initializeApp} from "firebase/app"
 import "firebase/database"
 import { getDatabase, ref, onValue } from 'firebase/database';
 
-
-
-
 export default function App() {
 
-    const [data, setData] = useState([])
-    const mapping = {"cnn": "CNN", "fox-news": "Fox News", "nbc-news": "NBC News", "bbc-news": "BBC News", "npr": "NPR"}
+    const [data, setData] = useState<CardProps[]>([]);
+    const mapping = {"cnn": "CNN", "fox-news": "Fox News", "nbc-news": "NBC News", "bbc-news": "BBC News", "npr": "NPR"} as { [key: string]: string };
 
     useEffect(() => {
-        const firebaseConfig = {
-            apiKey: process.env.REACT_APP_apiKey,
-            authDomain: process.env.REACT_APP_authDomain,
-            databaseURL: process.env.REACT_APP_databaseURL,
-            projectId: process.env.REACT_APP_projectId,
-            storageBucket: process.env.REACT_APP_storageBucket,
-            messagingSenderId: process.env.REACT_APP_messagingSenderId,
-            appId: process.env.REACT_APP_appId
-        }
+      // Firebase's Configuration 
+      const firebaseConfig = {
+        apiKey: process.env.REACT_APP_apiKey,
+        authDomain: process.env.REACT_APP_authDomain,
+        databaseURL: process.env.REACT_APP_databaseURL,
+        projectId: process.env.REACT_APP_projectId,
+        storageBucket: process.env.REACT_APP_storageBucket,
+        messagingSenderId: process.env.REACT_APP_messagingSenderId,
+        appId: process.env.REACT_APP_appId
+      }
           
-        // Initialize Firebase
-        const app = initializeApp(firebaseConfig);
-        const db = getDatabase(app);
-        const articles = ref(db,"/day0")
-        onValue(articles, (snapshot) => {
-            const retData = snapshot.val();
-            let dataN:any = [];
-            for (let key in retData){
-                if(retData.hasOwnProperty(key)){
-                    const retObj = JSON.parse(retData[key]);
-                    
-                    var Obj = {source: mapping[retObj["source"]], title: retObj["title"], author: retObj["author"], url: retObj["url"], summary: retObj["text"]};
-                    dataN.push(Obj);
-                }
-            }
-            setData(dataN)
-          });  
-    },[])
+      // Initialize Firebase
+      const app = initializeApp(firebaseConfig);
+      const db = getDatabase(app);
+      
+      // Access database sub-branch  
+      const articles = ref(db,"/day0")
+
+      // Store data from database
+      onValue(articles, (snapshot) => {
+          const retData = snapshot.val();
+          let dataN:any = [];
+          for (let key in retData){
+              if(retData.hasOwnProperty(key)){
+                  const retObj = JSON.parse(retData[key]);
+                  var Obj = {
+                    source: mapping[retObj["source"]], 
+                    title: retObj["title"], 
+                    author: retObj["author"], 
+                    url: retObj["url"], 
+                    summary: retObj["text"]};
+                  dataN.push(Obj);
+              }
+          }
+          setData(dataN)
+        });  
+  },[])
   
   return (
       <div>
@@ -57,7 +57,10 @@ export default function App() {
         <Row gutter={[16, 16]}>
           {data.map((news, index) => (
             <Col key={index} span={6}>
-              <NewsCard {...news} />
+              <NewsCard 
+              title={news.title}
+              summary={news.summary}
+              source={news.source}  />
             </Col>
           ))}
         </Row>
--- newsapp/src/component/NewsCard.tsx
+++ newsapp/src/component/NewsCard.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Card } from 'antd';
 
-// Define the interface for the props
-interface CardProps {
+// Define the interface for the card props
+export interface CardProps {
   title: string;
   summary: string;
   source: string;
-  width: number;
-  height: number;
 }
 
 // Define the Card component
-export const NewsCard: React.FC<CardProps> = ({ title, summary, source}) => {
+const NewsCard: React.FC<CardProps> = ({ title, summary, source}) => {
   return (
-    <Card className>
+    <Card>
       <h3>{title}</h3>
       <p>{summary}</p>
       <p>Source: {source}</p>
